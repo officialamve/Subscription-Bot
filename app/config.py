049193cd6fa2f463ccb9b96cbe67efaa4ccabcf4
Amal Mathew <amstudio678@gmail.com>
2026-02-25 18:33:09 +0530

    Fix config settings import

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGO_URI: str
+    DATABASE_NAME: str
+    ENCRYPTION_KEY: str
+    RAZORPAY_KEY_ID: str
+    RAZORPAY_KEY_SECRET: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
