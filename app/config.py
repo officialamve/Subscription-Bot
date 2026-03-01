4047983c7d35bacdb8f34bb92f4e497682fd62b0
Amal Mathew <amstudio678@gmail.com>
2026-03-01 07:38:53 +0530

    Added webhook secret to settings

--- app/config.py
+++ app/config.py
@@ -1,15 +1,14 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     MONGO_URI: str
     DATABASE_NAME: str
     ENCRYPTION_KEY: str
     RAZORPAY_KEY_ID: str
     RAZORPAY_KEY_SECRET: str
+    RAZORPAY_WEBHOOK_SECRET: str
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
